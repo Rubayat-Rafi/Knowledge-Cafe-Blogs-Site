8254bfa2f4df7fec1fa56ad803bc39c025991062
rubayat-rafi <rubayatrafi522@gmail.com>
2024-10-22 04:15:32 +0600

    bookmarks background color added

--- src/Component/Bookmark.jsx
+++ src/Component/Bookmark.jsx
@@ -5,8 +5,8 @@ const Bookmark = ({bookmark}) => {
     const {title} = bookmark;
     return(
         <section >
-        <div className='bg-slate-200 p-4 m-4 rounded-xl'>
-            <h3 className='text-2xl'>{title}</h3>
+        <div className='bg-white p-4 m-4 rounded-xl'>
+            <h3 className='text-lg'>{title}</h3>
         </div>
         </section>
     )
--- src/Component/Bookmarks.jsx
+++ src/Component/Bookmarks.jsx
@@ -1,15 +1,21 @@
 import Bookmark from "./Bookmark";
+import PropTypes from "prop-types";
 
 const Bookmarks = ({bookmarks}) => {
 
-    // console.log(bookmarks,"bookmarks");
+    console.log(bookmarks,"bookmarks");
     return(
-        <section className="w-1/3">
-            <h1 >Bookmarked Blogs: {bookmarks.length}</h1>
+        <section className="md:w-1/3 bg-[rgba(17,17,17,5%)] py-5 rounded-lg">
+            <h1 className="text-center text-2xl font-bold">Bookmarked Blogs: {bookmarks.length}</h1>
             {
                 bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
             }
         </section>
     )
 }
+
+Bookmarks.prototype = {
+    bookmarks: PropTypes.array,
+}
+
 export default Bookmarks;
